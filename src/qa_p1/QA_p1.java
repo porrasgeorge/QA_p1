c84b57673fa7fcb56da4ed1babd1f8afb9deb6c4
Jorge Porras <porrasgeorge@gmail.com>
2020-03-01 09:51:19 -0600

    first

---
+++ src/qa_p1/QA_p1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qa_p1;
+
+/**
+ *
+ * @author Administrador
+ */
+public class QA_p1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
