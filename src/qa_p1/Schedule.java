0d46b9d3df31c09a6f26a4c1de8a365b87b35be6
Jorge Porras <porrasgeorge@gmail.com>
2020-03-01 10:18:15 -0600

    Starting Schedule class

---
+++ src/qa_p1/Schedule.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qa_p1;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Schedule {
+    int ID;
+    int Bus_Number;
+    String Description;
+    LocalDate Departure_Time;
+    LocalDate Arrival_Time;
+    boolean Status;
+
+    public Schedule () {
+    
+    
+    }
+}
